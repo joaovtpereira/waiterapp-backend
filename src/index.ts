932767563c76ebabc85884583be330ac8c0fe993
João Vitor Pereira <joaovitorpessoal2@gmail.com>
2022-11-15 14:44:37 -0300

    create category and product

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "node:path";
+
+import { router } from "./router";
 
 const app = express();
 
@@ -7,6 +10,14 @@ mongoose
     .connect("mongodb://localhost:27017")
     .then(() => {
         const port = 3001;
+
+        app.use(
+            "/uploads",
+            express.static(path.resolve(__dirname, "..", "uploads"))
+        );
+        app.use(express.json());
+
+        app.use(router);
 
         app.listen(port, () => {
             console.log(`Server is running on http://localhost:${port}`);
---
+++ src/router.ts
@@ -0,0 +1,67 @@
+import path from "node:path";
+
+import { Router } from "express";
+import multer from "multer";
+
+import { createCategory } from "./app/useCases/categories/createCategory";
+import { listCategories } from "./app/useCases/categories/listCategories";
+import { createProducts } from "./app/useCases/products/createProduct";
+import { listProducts } from "./app/useCases/products/listProducts";
+import { listProductsByCategory } from "./app/useCases/categories/listProductsByCategory";
+
+export const router = Router();
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, callback) {
+            callback(null, path.resolve(__dirname, "..", "uploads"));
+        },
+        filename(req, file, callback) {
+            callback(null, `${Date.now()}-${file.originalname}`);
+        },
+    }),
+});
+
+// List categories
+
+router.get("/categories", listCategories);
+
+// Create category
+
+router.post("/categories", createCategory);
+
+// List product
+
+router.get("/products", listProducts);
+
+// Create product
+
+router.post("/products", upload.single("image"), createProducts);
+
+// Get products by categories
+
+router.get("/categories/:categoryId/products", listProductsByCategory);
+
+// List orders
+
+router.get("/orders", (req, res) => {
+    res.send("OK");
+});
+
+// Create order
+
+router.post("/orders", (req, res) => {
+    res.send("OK");
+});
+
+// Change order Status
+
+router.patch("/orders/:orderId", (req, res) => {
+    res.send("OK");
+});
+
+// Delete/cancel order
+
+router.delete("/orders/:orderId", (req, res) => {
+    res.send("OK");
+});
